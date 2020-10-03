1fc8edbc3c01bdbad4a2d712291a201534269462
Amin <amin.sakha00@gmail.com>
2020-10-03 18:49:36 +0330

    close to finish :)

---
+++ app/src/main/java/com/calculator/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.calculator
+
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import gr.net.maroulis.library.EasySplashScreen
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val easySplashScreenView: View = EasySplashScreen(this)
+            .withFullScreen()
+            .withTargetActivity(MainActivity::class.java)
+            .withSplashTimeOut(3500)
+            .withBackgroundResource(R.color.red)
+            .withBeforeLogoText("CalQlater")
+            .withLogo(R.drawable.icon)
+            .withAfterLogoText("Powered by M.A.S")
+            .create()
+        setContentView(easySplashScreenView)
+    }
+}
